1fc72d90f77e99e5171bcaa255723f58d55014dc
freya02 <41875020+freya022@users.noreply.github.com>
2022-03-30 23:52:08 +0200

    Modifications calcul score

--- src/main/java/com/freya02/quadropolis/Player.java
+++ src/main/java/com/freya02/quadropolis/Player.java
@@ -3,6 +3,7 @@ package com.freya02.quadropolis;
 import com.freya02.quadropolis.plate.Building;
 import com.freya02.quadropolis.plate.BuildingType;
 import com.freya02.quadropolis.plate.PlayerPlate;
+import com.freya02.quadropolis.plate.Tile;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
@@ -37,7 +38,16 @@ public class Player {
 
 	public void calculScore() {
 		int score = 0;
-		PlayerPlate PP = Quadropolis.getInstance().getPlayers().get(1).getPlate();
+
+		//Vu qu'on est dans l'objet joueur, on a déjà son plateau, pas besoin de la récupérer dans Quadropolis
+		score += getHouseScore();
+		//...
+
+		score += getPortScore();
+
+		//TODO remplacer le reste des calculs par des fonctions individuelles
+
+		PlayerPlate PP = getPlate();
 		ArrayList<Building> quartier1= new ArrayList<Building>((Collection<? extends Building>) Arrays.asList(PP.get(0,0),PP.get(1,0),PP.get(0,1),PP.get(1,1)));
 		ArrayList<Building> quartier2= new ArrayList<Building>((Collection<? extends Building>) Arrays.asList(PP.get(2,0),PP.get(3,0),PP.get(2,1),PP.get(3,1)));
 		ArrayList<Building> quartier3= new ArrayList<Building>((Collection<? extends Building>) Arrays.asList(PP.get(0,2),PP.get(1,2),PP.get(1,3),PP.get(0,3)));
@@ -160,6 +170,79 @@ public class Player {
 		}
 
 		this.setScore(score);
+	}
+
+	private int getHouseScore() {
+		int score = 0;
+
+		for (Tile tile : plate.getTiles()) { //Pas besoin de savoir le x ou le y ici, on veut juste savoir le nombre de stack de chaque maison
+			if (tile instanceof Building building && building.getBuildingType() == BuildingType.HOUSE) { //Si c'est une maison
+				score += switch (building.getStackCount()) { //Voir la fiche de score pour les habitations, page 5
+					case 1 -> 1;
+					case 2 -> 3;
+					case 3 -> 6;
+					case 4 -> 10;
+					default -> throw new IllegalArgumentException("A house has more than 4 stacks"); //Au cas où
+				};
+			}
+		}
+
+		return score;
+	}
+
+	private int getPortScore() {
+		//On recherche la succession de port la plus longue, en ligne ou colonne
+
+		int longest = 0;
+
+		//On regarde chaque case, si on trouve un port alors on cherche une ligne / colonne
+		for (int baseX = 0; baseX < plate.getWidth(); baseX++) {
+			for (int baseY = 0; baseY < plate.getHeight(); baseY++) {
+				final Tile tile = plate.get(baseX, baseY);
+
+				if (tile instanceof Building building && building.getBuildingType() == BuildingType.PORT) {
+					{
+						//Regardons les colonnes en premier
+						int length = 0;
+						for (int y = baseY; y < plate.getHeight(); y++) {
+							final Tile otherTile = plate.get(baseX, y);
+
+							if (otherTile instanceof Building otherBuilding && otherBuilding.getBuildingType() == BuildingType.PORT) {
+								length++;
+							} else {
+								break;
+							}
+						}
+
+						longest = Math.max(longest, length);
+					}
+
+					{
+						//Regardons les lignes après
+						int length = 0;
+						for (int x = baseX; x < plate.getWidth(); x++) {
+							final Tile otherTile = plate.get(x, baseY);
+
+							if (otherTile instanceof Building otherBuilding && otherBuilding.getBuildingType() == BuildingType.PORT) {
+								length++;
+							} else {
+								break;
+							}
+						}
+
+						longest = Math.max(longest, length);
+					}
+				}
+			}
+		}
+
+		return switch (longest) {
+			case 1 -> 0;
+			case 2 -> 3;
+			case 3 -> 7;
+			case 4 -> 12;
+			default -> throw new IllegalStateException("Unexpected value: " + longest);
+		};
 	}
 
 	public int getPlayerNum() {
